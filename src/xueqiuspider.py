a285864f667be999c86a17d8db9f5e7e65c74663
lzk20182000 <1749977097@qq.com>
2021-04-28 10:26:26 +0800

    change xueqiuspider.py

--- src/xueqiuspider.py
+++ src/xueqiuspider.py
@@ -16,10 +16,13 @@ headers = {
             "Cookie": "device_id=1bc3f7e62a55294135b256919bad7df4; s=c814xxmy21; bid=55919114a627b9cdf7c2cf6d3b021c7e_km95yxdt; acw_tc=2760820616190675986288295e4abf1a86f8125c58b7e47386a040d5cdea12; Hm_lvt_1db88642e346389874251b5a1eded6e3=1619067601; xq_a_token=4296d3528edb68f8e29363040c82486510a38e74; xqat=4296d3528edb68f8e29363040c82486510a38e74; xq_r_token=bdb802dc27dfb8a683ced0c98ce059de3af61b85; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOjY4NzIyNzEzNzcsImlzcyI6InVjIiwiZXhwIjoxNjIxNjU5NjIyLCJjdG0iOjE2MTkwNjc2MjIxNTksImNpZCI6ImQ5ZDBuNEFadXAifQ.mfm1pkInkb6__-yifL7xWMOdV8Sl5PZawnsKnRv1kTVy66FUQ8S2CTbLVN2MacjulkY6hSU_SBVeQhNvrj832TvU9pDPG0xoH4pWPlWCB3MgafEo2fPVrN_lzlwcBlFjLfJKnHfZWCyRnTrqXCreO7iEgMbL3CLj10tNSc3CwfowOW-xzM0vNBoU18YzAV2cnYBEPE9kEAJZPB9NT5cpLqgiyo2LxLPUAgr5_5oZQiGpbA5MN7sxKKuDsbJxRolfIH66QIMHdXlZnTnool1YYoyV6C3juzSp5jWjDMJ0TynIrbxTJHuSzbk4CUZcs2-MaXC6dClOiBO8A2_7s37toQ; xq_is_login=1; u=6872271377; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1619067675"
 }
 def save_file(content_list):
-    for content in content_list:
-        with open('xueqiu.json', 'a')as f:
-            f.write(str(content).encode("gbk", 'ignore').decode("gbk", "ignore"))
+    with open('xueqiu.json', 'w')as f:
+        f.write("{ \n""name:[ \n")
+    with open('xueqiu.json', 'a')as f:
+        for content in content_list:
+            f.write('\t' + str(content).encode("gbk", 'ignore').decode("gbk", "ignore") + ',')
             f.write("\n")
+        f.write("\n     ]\n}")
             
 def parse_comment_url( url):
     r = requests.get(url, headers=headers, proxies=proxies, verify=False)  # 发送请求
@@ -34,7 +37,7 @@ def parse_comment_url( url):
     return content_list, count
 
 stock_num = 'SH601318'  # 输入股票代码
-for i in range(100):
+for i in range(5):
     detail_url = "https://xueqiu.com/statuses/search.json?count=10&comment=0&symbol={}&hl=0&source=all&sort=&page={}&q=&type=11".format(stock_num, i + 1)
     print("正爬取"+detail_url)
     try:
