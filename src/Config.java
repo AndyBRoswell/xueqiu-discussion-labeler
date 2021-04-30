099f98a24309c1312b2775b26f5057a87d1fcdf3
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-04-30 23:56:35 +0800

    Update Config.java

--- src/Config.java
+++ src/Config.java
@@ -1,6 +1,12 @@
 import java.io.*;
 import java.util.regex.*;
 import javax.xml.parsers.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.*;
 
 import org.w3c.dom.*;
@@ -13,8 +19,13 @@ public class Config {
 	static final XPathFactory xfactory = XPathFactory.newInstance();
 	static final XPath xpath = xfactory.newXPath();
 	static final Pattern ShellVariablePattern = Pattern.compile("\\$[\\w]+[^\\w]");
+	static final TransformerFactory tfactory = TransformerFactory.newInstance();
+	static Transformer transformer;
+	static DOMSource source;
 
 	static {
+		try { transformer = tfactory.newTransformer(); }
+		catch (TransformerConfigurationException e) { e.printStackTrace(); }
 		try { builder = dfactory.newDocumentBuilder(); }
 		catch (ParserConfigurationException e) { e.printStackTrace(); }
 	}
@@ -31,11 +42,16 @@ public class Config {
 		return EvalResult.item(0).getTextContent();
 	}
 
-	public static void ModifySingleConfigEntry(String xpathexpr, String content) {
-
+	public static void ModifySingleConfigEntry(String xpathexpr, String content) throws XPathExpressionException {
+		XPathExpression expr = xpath.compile(xpathexpr);
+		NodeList EvalResult = (NodeList) expr.evaluate(ConfigXML, XPathConstants.STRING);
+		EvalResult.item(0).setTextContent(content);
 	}
 
-	public static void SaveConfig(String pathname) {
-
+	public static void SaveConfig(String pathname) throws TransformerException {
+		source = new DOMSource(ConfigXML);
+		File cfgxmlfile = new File(pathname);
+		StreamResult result = new StreamResult(cfgxmlfile);
+		transformer.transform(source,result);
 	}
 }
