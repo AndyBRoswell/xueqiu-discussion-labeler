2d46501d9edfcc07989eedd7c67b52994290b19f
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-04-30 22:09:50 +0800

    /

--- src/Global.java
+++ src/Global.java
@@ -1,4 +1,5 @@
 public class Global {
 	public static final String AppPath = System.getProperty("user.dir");
+	public static final String ConfigPath = AppPath + "\\cfg";
 	public static final String IconPath = AppPath + "\\Icon";
 }
