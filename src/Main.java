2f082750c0bc2cc65d56e847d5c1aa4ad1ddc31a
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-05-01 21:40:39 +0800

    added config module

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import org.xml.sax.SAXException;
 
+import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) throws IOException, SAXException, XPathExpressionException {
+	public static void main(String[] args) throws IOException, SAXException, XPathExpressionException, TransformerException {
 		GUI MainForm = new GUI();
-		System.out.println(Global.AppPath);
-		System.out.println(Global.DefaultSavePath);
-		Config.LoadConfig(Global.DefaultConfig);
-		System.out.println(Config.QuerySingleConfigEntry("/config/storage/import-and-export/default-export-dir"));
+//		Config.LoadConfig(Global.DefaultConfig);
+//		System.out.println(Config.QuerySingleConfigEntry("/config/storage/import-and-export/default-export-dir"));
+//		Config.ModifySingleConfigEntry("/config/storage/import-and-export/default-export-dir", "D:\\Temp");
+//		System.out.println(Config.QuerySingleConfigEntry("/config/storage/import-and-export/default-export-dir"));
+//		Config.SaveConfig(Global.DefaultConfig);
 	}
 }
