3cfe8daf7a9106eba4205387d13f5eea2ce0df49
A ling <907611914@qq.com>
2021-05-03 00:54:42 +0800

    .

--- src/GUI.java
+++ src/GUI.java
@@ -5,28 +5,105 @@ import javax.swing.table.JTableHeader;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 public class GUI extends JFrame {
 	JFrame frame = new JFrame("雪球网股票评论");
-	Container contentPane = frame.getContentPane();
+	String[] title = { "股票编号", "评论", "已选标签" };
+	Object[][] rowData = {
+			{ 1, 2, 3 },
+			{ 1, 2, 3 }
+	};
+	/*按钮*/
+	JButton ButtonDownLoad = new JButton();
+	JButton AddTagButton = new JButton();
+	JButton addTag1 = new AddButton();
+	JButton addTag2 = new AddButton();
+	JButton addTag3 = new AddButton();
+	JButton addTag4 = new AddButton();
+	/*搜索栏*/
+	JTextField search = new JTextField(6);
+	/*勾选框*/
+	JCheckBox Marked = new JCheckBox("已标注");
+	JCheckBox UnMarked = new JCheckBox("未标注");
+	/*标签*/
+	JLabel ChooseTag = new JLabel("可选标注");
+	JLabel Tendency = new JLabel("短期趋势:");
+	JLabel up = new LabelSet("看涨", Color.LIGHT_GRAY);
+	JLabel down = new LabelSet("看跌", Color.LIGHT_GRAY);
+
+	JLabel Relevance = new JLabel("相关联性:");
+	JLabel Relative = new LabelSet("有关", Color.LIGHT_GRAY);
+	JLabel UnRelative = new LabelSet("无关", Color.LIGHT_GRAY);
+
+	JLabel Evaluation = new JLabel("总体评价:");
+	JLabel good = new LabelSet("好评", Color.LIGHT_GRAY);
+	JLabel middle = new LabelSet("中评", Color.LIGHT_GRAY);
+	JLabel bad = new LabelSet("差评", Color.LIGHT_GRAY);
+
+	JLabel Advertisement = new JLabel("推广情况:");
+	JLabel have = new LabelSet("有", Color.LIGHT_GRAY);
+	JLabel havenot = new LabelSet("无", Color.LIGHT_GRAY);
+	/*表格*/
+	JTable table = new JTable(rowData, title);
+	JTableHeader tableHeader = table.getTableHeader();
+	JScrollPane scrollPane = new JScrollPane(table);
+
+	/*字体*/
 	public GUI() {
-		contentPane.setLayout(new BorderLayout());
 		frame.setLocation(500, 250);          //窗口显示位置
 		frame.setSize(1000, 600);      //窗口大小
 		init();
-		frame.pack();
+		frame.addComponentListener(new ComponentListener() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				int x=frame.getWidth();
+				int y=frame.getHeight();
+				Font font = new Font("微软雅黑", 0, x/75);
+				search.setBounds(10,5,x*3/4,y/25);
+				Marked.setBounds(10+x*9/12,5,x/13,y/25);
+				Marked.setFont(font);
+				UnMarked.setFont(font);
+				UnMarked.setBounds(10+x*10/12,5,x/13,y/25);
+				ButtonDownLoad.setBounds(x*19/20,5,y/30,y/30);
+				scrollPane.setBounds(10,y/25+5,x-35,y*6/8);
+				table.setBounds(10,y/25+5,x-35,y*6/8);
+				ChooseTag.setBounds(10,y*8/10-5,x/13,30);
+				ChooseTag.setFont(font);
+				AddTagButton.setBounds(12,y*10/12,x/30,x/30);
+				Tendency.setFont(font);
+				up.setFont(font);
+				down.setFont(font);
+				Relative.setFont(font);
+				Relevance.setFont(font);
+				UnRelative.setFont(font);
+				Evaluation.setFont(font);
+				good.setFont(font);
+				middle.setFont(font);
+				bad.setFont(font);
+				Advertisement.setFont(font);
+				have.setFont(font);
+				havenot.setFont(font);
+				Tendency.setBounds(AddTagButton.getX()+AddTagButton.getWidth()+x/60,y*10/12,x/17,y/25);
+				up.setBounds(Tendency.getX()+Tendency.getWidth(),y*10/12,x/25,y/25);
+				down.setBounds(up.getX()+up.getWidth()+x/200,y*10/12,x/25,y/25);
+				addTag1.setBounds(down.getX()+down.getWidth()+x/200,y*10/12,y/25,y/25);
+
+			}
+			@Override public void componentMoved(ComponentEvent e) { }
+			@Override public void componentShown(ComponentEvent e) { }
+			@Override public void componentHidden(ComponentEvent e) { }
+		});
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
-
 	private void init() {
 		/*图标*/
 		Icon iconDownload = new ImageIcon(Global.IconPath + "\\download.png");
 		Icon iconAdd = new ImageIcon(Global.IconPath + "\\addplus.png");
 		/*按钮*/
-		JButton ButtonDownLoad = new JButton();
-		ButtonDownLoad.setPreferredSize(new Dimension(40, 40));
 		ButtonDownLoad.setIcon(iconDownload);
 		ButtonDownLoad.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) { new GUIDownLoad(); }});
 		/*一级菜单创建*/
@@ -43,12 +120,13 @@ public class GUI extends JFrame {
 		/*文件子菜单*/
 		JMenuItem ImportMenuItem = new JMenuItem("导入");
 		JMenuItem ExportMenuItem = new JMenuItem("导出");
-		JMenuItem JournalMenuItem = new JMenuItem("日志");
 		JMenuItem SetMenuItem = new JMenuItem("设置");
+		JMenuItem JournalMenuItem = new JMenuItem("日志");
 		JMenuItem ExitMenuItem = new JMenuItem("退出");
 		FileMenu.add(ImportMenuItem);
 		FileMenu.add(ExportMenuItem);
 		FileMenu.add(JournalMenuItem);
+		FileMenu.add(SetMenuItem);
 		FileMenu.add(SetMenuItem);
 		FileMenu.addSeparator();
 		FileMenu.add(ExitMenuItem);
@@ -73,104 +151,44 @@ public class GUI extends JFrame {
 			public void menuDeselected(MenuEvent e) { }
 			public void menuCanceled(MenuEvent e) { }
 		});
-		/*panel1：搜索框、勾选框、任务列表按钮*/
-		JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		JTextField search = new JTextField(70);
-		JCheckBox Marked = new JCheckBox("已标注");
-		JCheckBox UnMarked = new JCheckBox("未标注");
-		panel1.add(search);
-		panel1.add(Marked);
-		panel1.add(UnMarked);
-		panel1.add(ButtonDownLoad);
-		/*panel2：评论表格*/
-		JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		String[] title = { "股票编号", "评论", "已选标签" };
-		Object[][] rowData = {
-				{ 1, 2, 3 },
-				{ 1, 2, 3 }
-		};
-		JTable table = new JTable(rowData, title);
-		JTableHeader tableHeader = table.getTableHeader();
-		JScrollPane scrollPane = new JScrollPane(table);
-		table.setPreferredScrollableViewportSize(new Dimension((frame.getWidth()) - 50, (frame.getHeight()) - 220));
 		table.setRowHeight(30);
 		tableHeader.setFont(new Font("微软雅黑", Font.PLAIN, 16));
 		tableHeader.setResizingAllowed(false);               // 设置不允许手动改变列宽
 		tableHeader.setReorderingAllowed(false);
 		scrollPane.setViewportView(table);
-		panel2.add(scrollPane);
-		/*panel3:可选标注*/
-		JPanel panel3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JPanel TagPanel = new JPanel();
-		TagPanel.setLayout(new BorderLayout());
-		JLabel ChooseTag = new JLabel("可选标注");
-		TagPanel.add(ChooseTag, BorderLayout.NORTH);
-		JButton AddTagButton = new JButton();
-		AddTagButton.setPreferredSize(new Dimension(40, 40));
+
 		AddTagButton.setIcon(iconAdd);
-		TagPanel.add(AddTagButton, BorderLayout.SOUTH);
 		AddTagButton.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) { new GUIAddTagSort(); }});
-		panel3.add(TagPanel);
-		/*panel4:标注栏*/
-		JLabel Evaluation = new JLabel("总体评价");
-		JLabel Advertisement = new JLabel("推广情况");
-		JLabel Relevance = new JLabel("相关联性");
-		JLabel Tendency = new JLabel("短期趋势");
-		JPanel panel4 = new JPanel();
-		panel4.setLayout(new BoxLayout(panel4, BoxLayout.X_AXIS));
-		/*TPanel1：总体评价*/
-		JPanel TPanel1 = new JPanel(new FlowLayout());
-		JLabel good = new LabelSet("好评", Color.LIGHT_GRAY);
-		JLabel middle = new LabelSet("中评", Color.LIGHT_GRAY);
-		JLabel bad = new LabelSet("差评", Color.LIGHT_GRAY);
-		JButton addTag1 = new AddButton();
 		addTag1.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) { new GUIAddTag(); }});
-		TPanel1.add(Evaluation);
-		TPanel1.add(good);
-		TPanel1.add(middle);
-		TPanel1.add(bad);
-		TPanel1.add(addTag1);
-		/*TPanel2：推广情况*/
-		JPanel TPanel2 = new JPanel(new FlowLayout());
-		JLabel have = new LabelSet("有", Color.LIGHT_GRAY);
-		JLabel havenot = new LabelSet("无", Color.LIGHT_GRAY);
-		JButton addTag2 = new AddButton();
 		addTag2.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) { new GUIAddTag(); }});
-		TPanel2.add(Advertisement);
-		TPanel2.add(have);
-		TPanel2.add(havenot);
-		TPanel2.add(addTag2);
-		/*TPanel3：相关联性*/
-		JPanel TPanel3 = new JPanel(new FlowLayout());
-		JLabel Relative = new LabelSet("有关", Color.LIGHT_GRAY);
-		JLabel UnRelative = new LabelSet("无关", Color.LIGHT_GRAY);
-		JButton addTag3 = new AddButton();
 		addTag3.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) { new GUIAddTag(); }});
-		TPanel3.add(Relevance);
-		TPanel3.add(Relative);
-		TPanel3.add(UnRelative);
-		TPanel3.add(addTag3);
-		/*TPanel4：短期趋势*/
-		JPanel TPanel4 = new JPanel(new FlowLayout());
-		JLabel up = new LabelSet("看涨", Color.LIGHT_GRAY);
-		JLabel down = new LabelSet("看跌", Color.LIGHT_GRAY);
-		JButton addTag4 = new AddButton();
 		addTag4.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) { new GUIAddTag(); }});
-		TPanel4.add(Tendency);
-		TPanel4.add(up);
-		TPanel4.add(down);
-		TPanel4.add(addTag4);
-		/*添加Tpanel到标注栏Panel4*/
-		panel4.add(TPanel1);
-		panel4.add(TPanel2);
-		panel4.add(TPanel3);
-		panel4.add(TPanel4);
 
-		panel3.add(panel4);
-
-		contentPane.add(panel1, BorderLayout.PAGE_START);
-		contentPane.add(panel2, BorderLayout.CENTER);
-		contentPane.add(panel3, BorderLayout.SOUTH);
+		frame.getContentPane().setLayout(null);
+		frame.add(search);
+		frame.add(Marked);
+		frame.add(UnMarked);
+		frame.add(ButtonDownLoad);
+		frame.add(scrollPane);
+		frame.add(ChooseTag);
+		frame.add(AddTagButton);
+		frame.add(Tendency);
+		frame.add(up);
+		frame.add(down);
+		frame.add(Relative);
+		frame.add(Relevance);
+		frame.add(UnRelative);
+		frame.add(Evaluation);
+		frame.add(good);
+		frame.add(middle);
+		frame.add(bad);
+		frame.add(Advertisement);
+		frame.add(have);
+		frame.add(havenot);
+		frame.add(addTag1);
+		frame.add(addTag2);
+		frame.add(addTag3);
+		frame.add(addTag4);
 	}
 
 	public static class LabelSet extends JLabel {
@@ -178,7 +196,6 @@ public class GUI extends JFrame {
 			super(text);
 			setOpaque(true);
 			setBackground(bgColor);
-			setPreferredSize(new Dimension(40, 20));
 			setHorizontalAlignment(SwingConstants.CENTER);
 		}
 	}
