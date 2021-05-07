904a6bf436da21338b7e6d449747a41c8b0f5706
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-05-07 23:16:40 +0800

    adding storage accessor

--- src/DataManipulator.java
+++ src/DataManipulator.java
@@ -9,45 +9,49 @@ public class DataManipulator {
 }
 
 class DiscussionItem {
-	private String Username;
-	private String UserHomePage;
-	private boolean Modified;
-	private String Client;
-	private LocalDateTime DateTime;
-	private int NumberOfForwards;
-	private int NumberOfComments;
-	private int NumberOfLikes;
+//	private String Username;
+//	private String UserHomePage;
+//	private boolean Modified;
+//	private String Client;
+//	private LocalDateTime DateTime;
+//	private int NumberOfForwards;
+//	private int NumberOfComments;
+//	private int NumberOfLikes;
 
 	private String Text;
 	private ConcurrentHashMap<String, String> Label;
 
-	DiscussionItem(String Username, String UserHomePage, boolean Modified, String Client, LocalDateTime DateTime, int NumberOfForwards, int NumberOfComments, int NumberOfLikes, String Text) {
-		this.Username = Username;
-		this.UserHomePage = UserHomePage;
-		this.Modified = Modified;
-		this.Client = Client;
-		this.DateTime = DateTime;
-		this.NumberOfForwards = NumberOfForwards;
-		this.NumberOfComments = NumberOfComments;
-		this.NumberOfLikes = NumberOfLikes;
+//	DiscussionItem(String Username, String UserHomePage, boolean Modified, String Client, LocalDateTime DateTime, int NumberOfForwards, int NumberOfComments, int NumberOfLikes, String Text) {
+//		this.Username = Username;
+//		this.UserHomePage = UserHomePage;
+//		this.Modified = Modified;
+//		this.Client = Client;
+//		this.DateTime = DateTime;
+//		this.NumberOfForwards = NumberOfForwards;
+//		this.NumberOfComments = NumberOfComments;
+//		this.NumberOfLikes = NumberOfLikes;
+//		this.Text = Text;
+//	}
+
+	DiscussionItem(String Text) {
 		this.Text = Text;
 	}
 
-	String GetUsername() { return Username; }
-
-	String GetUserHomePage() { return UserHomePage; }
-
-	boolean IsModified() { return Modified; }
-
-	String GetClient() { return Client; }
-
-	LocalDateTime GetDateTime() { return DateTime; }
-
-	int GetNumberOfForwards() { return NumberOfForwards; }
-
-	int GetNumberOfComments() { return NumberOfComments; }
-
-	int GetNumberOfLikes() { return NumberOfLikes; }
+//	String GetUsername() { return Username; }
+//
+//	String GetUserHomePage() { return UserHomePage; }
+//
+//	boolean IsModified() { return Modified; }
+//
+//	String GetClient() { return Client; }
+//
+//	LocalDateTime GetDateTime() { return DateTime; }
+//
+//	int GetNumberOfForwards() { return NumberOfForwards; }
+//
+//	int GetNumberOfComments() { return NumberOfComments; }
+//
+//	int GetNumberOfLikes() { return NumberOfLikes; }
 
 	String GetText() { return Text; }
 
