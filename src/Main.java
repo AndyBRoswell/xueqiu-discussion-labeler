d3c713460962fed3f102ed096271a0d9e2aa7248
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-05-07 23:17:14 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
-import org.xml.sax.SAXException;
-
-import javax.xml.transform.TransformerException;
-import javax.xml.xpath.XPathExpressionException;
-import java.io.IOException;
-
 public class Main {
-	public static void main(String[] args) throws IOException, SAXException, XPathExpressionException, TransformerException {
+	public static void main(String[] args) {
 		GUI MainForm = new GUI();
-		GUIConfig guiConfig=new GUIConfig();
+		GUIConfig guiConfig = new GUIConfig();
 	}
 }
