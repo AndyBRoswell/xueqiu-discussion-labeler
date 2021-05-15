51cb8019301a44c4b90ef8bea6aca261ed33e3e2
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-05-15 23:47:54 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,16 @@ public class Main {
 		GUI MainForm = new GUI();
 //		GUIConfig guiConfig = new GUIConfig(Global.DefaultConfig);
 		Config.LoadConfig(Global.DefaultConfig);
-		StorageAccessor.LoadAllAvailableLabels();
-		for (Map.Entry<String, ArrayList<String>> entry : DataManipulator.AllLabels.entrySet()) {
-			System.out.println(entry.toString());
-		}
-		StorageAccessor.SaveAllAvailableLabels();
-		StorageAccessor.LoadDiscussionFromCSV(Global.DefaultSavePath + "\\Book1.csv", "GB2312");
-		for (DiscussionItem entry : DataManipulator.DiscussionList) {
-			System.out.println(entry.GetText());
-			System.out.println(entry.GetLabels().toString());
-		}
-		StorageAccessor.SaveDiscussionToCSV(Global.DefaultSavePath + "\\Book2.csv", "GB2312");
+//		StorageAccessor.LoadAllAvailableLabels();
+//		for (Map.Entry<String, ArrayList<String>> entry : DataManipulator.AllLabels.entrySet()) {
+//			System.out.println(entry.toString());
+//		}
+//		StorageAccessor.SaveAllAvailableLabels();
+//		StorageAccessor.LoadDiscussionFromCSV(Global.DefaultSavePath + "\\Book1.csv", "GB2312");
+//		for (DiscussionItem entry : DataManipulator.DiscussionList) {
+//			System.out.println(entry.GetText());
+//			System.out.println(entry.GetLabels().toString());
+//		}
+//		StorageAccessor.SaveDiscussionToCSV(Global.DefaultSavePath + "\\Book2.csv", "GB2312");
 	}
 }
