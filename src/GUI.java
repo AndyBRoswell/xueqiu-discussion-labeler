7583fb2315bf09a61f35487fa72312594cb544e0
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-05-25 22:24:39 +0800

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -27,7 +27,7 @@ public class GUI extends JFrame {
 	JFrame MainFrame = new JFrame("雪球网股票评论");
 	Font font = new Font("微软雅黑", Font.PLAIN, Global.FontSizeD);
 
-	ArrayList<ArrayList<String>> LabelData = new ArrayList<>(); // 标签类及标签的名称
+	ArrayList<ArrayList<String>> LabelsAndCategories = new ArrayList<>(); // 标签类及标签的名称
 	ArrayList<ArrayList<ConcreteLabelControl>> LabelControls = new ArrayList<>();
 	ArrayList<LabelCategoryControl> LabelCategoryControls = new ArrayList<>();
 	ArrayList<AddButton> AddLabelButtons = new ArrayList<>();
@@ -245,24 +245,24 @@ public class GUI extends JFrame {
 			String str;
 			int RowCount = 0;
 			while ((str = br.readLine()) != null) {
-				LabelData.add(new ArrayList<String>());
+				LabelsAndCategories.add(new ArrayList<String>());
 				String[] Data = str.split(" "); // 分离该行的标签类与标签，空格隔开
 				for (int i = 0; i < Data.length; i++) {
-					LabelData.get(RowCount).add(Data[i]); // 将该标签类及其含有的标签内容放入数据结构
+					LabelsAndCategories.get(RowCount).add(Data[i]); // 将该标签类及其含有的标签内容放入数据结构
 				}
 				RowCount++;
 			}
 			br.close();
-			for (int i = 0; i < LabelData.size(); i++) {
-				LabelCategoryControls.add(new LabelCategoryControl(LabelData.get(i).get(0))); // 添加类名标签控件到数据结构
+			for (int i = 0; i < LabelsAndCategories.size(); i++) {
+				LabelCategoryControls.add(new LabelCategoryControl(LabelsAndCategories.get(i).get(0))); // 添加类名标签控件到数据结构
 				AllLabelsPanel.add(LabelCategoryControls.get(i)); // 添加标签类到主界面
 				String text = String.valueOf(i); // int to string
 				AddLabelButtons.add(new AddButton(text)); // 产生该类标签的添加按钮并添加到数据结构
 				AddLabelButtons.get(AddLabelButtons.size() - 1).setIcon(iconAddSmall);
 				AllLabelsPanel.add(AddLabelButtons.get(i)); // 将该类标签的添加按钮添加到主界面
 				ArrayList<ConcreteLabelControl> Label = new ArrayList<>(); // 该类股评标签控件的集合
-				for (int j = 1; j < LabelData.get(i).size(); j++) {
-					Label.add(new ConcreteLabelControl(LabelData.get(i).get(j), Color.GRAY)); // 添加新的股评标签控件到数据结构
+				for (int j = 1; j < LabelsAndCategories.get(i).size(); j++) {
+					Label.add(new ConcreteLabelControl(LabelsAndCategories.get(i).get(j), Color.GRAY)); // 添加新的股评标签控件到数据结构
 				}
 				LabelControls.add(Label); // 添加该类股评标签的控件到数据结构
 			}
@@ -370,23 +370,22 @@ public class GUI extends JFrame {
 			AllLabelsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 //			AllLabelsScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 //			AllLabelsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-			int num = 0;
+			int LabelControlsAdded = 0;
 			for (int i = 0; i < LabelCategoryControls.size(); i++) {
 				LabelCategoryControls.get(i).setFont(font);
 				if (i == 0) {
 					LabelCategoryControls.get(i).setBounds(0, X / 64, X / 17, Y / 25); // 第一个标签类控件
 				}
 				else { // 第二个及以后的标签类控件
-					num += LabelData.get(i - 1).size() - 1; // 在添加该标签类控件时，累计已经添加的子标签控件数量
-					LabelCategoryControls.get(i).setBounds(i * X / 17 + num * X / 23 + i * TaskListButton.getWidth(), X / 64, X / 17, Y / 25);
+					LabelControlsAdded += LabelsAndCategories.get(i - 1).size() - 1; // 在添加该标签类控件时，累计已经添加的子标签控件数量（减去标签类一个）
+					LabelCategoryControls.get(i).setBounds(i * X / 17 + LabelControlsAdded * X / 23 + i * TaskListButton.getWidth(), X / 64, X / 17, Y / 25);
 				}
 				for (int j = 0; j < LabelControls.get(i).size(); j++) { //每个子标签控件的坐标为：i*x/17+num*x/23+x/17+(j-1)*x/23
-					LabelControls.get(i).get(j).setBounds(i * X / 17 + num * X / 23 + X / 17 + j * X / 23 + i * TaskListButton.getWidth(), X / 64, X / 25, Y / 25);
+					LabelControls.get(i).get(j).setBounds(i * X / 17 + LabelControlsAdded * X / 23 + X / 17 + j * X / 23 + i * TaskListButton.getWidth(), X / 64, X / 25, Y / 25);
 					if (j == LabelControls.get(i).size() - 1) {
-						AddLabelButtons.get(i).setBounds(i * X / 17 + num * X / 23 + X / 17 + (j + 1) * X / 23 + i * TaskListButton.getWidth(), X / 64, TaskListButton.getWidth(), TaskListButton.getHeight());
-//						AddLabelButton.get(i).setIcon(iconAddSmall);
+						AddLabelButtons.get(i).setBounds(i * X / 17 + LabelControlsAdded * X / 23 + X / 17 + (j + 1) * X / 23 + i * TaskListButton.getWidth(), X / 64, TaskListButton.getWidth(), TaskListButton.getHeight());
 						if (i == LabelCategoryControls.size() - 1) {
-							AllLabelsPanel.setPreferredSize(new Dimension(i * X / 17 + num * X / 23 + X / 17 + (j + 1) * X / 23 + (i + 1) * TaskListButton.getWidth(), X / 16));
+							AllLabelsPanel.setPreferredSize(new Dimension(i * X / 17 + LabelControlsAdded * X / 23 + X / 17 + (j + 1) * X / 23 + (i + 1) * TaskListButton.getWidth(), X / 16));
 						}
 					}
 				}
