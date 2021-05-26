27d7e7589b36517916c226ecd0e989ae09d28424
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-05-27 00:24:20 +0800

    test data

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws XPathExpressionException, IOException, SAXException, InterruptedException {
-//		GUI MainForm = new GUI();
+		GUI MainForm = new GUI();
 
 		/*读取全部可用标注*/
 //		StorageAccessor.LoadAllAvailableLabels();
@@ -17,7 +17,7 @@ public class Main {
 //		StorageAccessor.SaveAllAvailableLabels();
 
 		/*读取指定文件中的股票讨论及其标注*/
-		StorageAccessor.LoadDiscussionFromCSV(Global.DefaultSavePath + "\\Book1.csv", "gbk", 0);
+//		StorageAccessor.LoadDiscussionFromCSV(Global.DefaultSavePath + "\\Book1.csv", "gbk", 0);
 //		StorageAccessor.LoadDiscussionFromCSV(Global.DefaultSavePath + "\\中国平安.csv", "gbk", 1);
 //		for (DiscussionItem entry : DataManipulator.DiscussionList) {
 //			System.out.println(entry.GetText());
@@ -31,14 +31,14 @@ public class Main {
 		/*搜索功能*/
 //		DataManipulator.Search(2, null, null);
 //		DataManipulator.Search(0, new String[]{ "2A" }, null);
-		DataManipulator.Search(0, null, new String[]{ "是" });
+//		DataManipulator.Search(0, null, new String[]{ "是" });
 //		DataManipulator.Search(0, new String[]{ "梭哈" }, null);
 
 		/*最终的搜索结果*/
 //		System.out.println(DataManipulator.FinalSearchResult.size());
-		for (int i : DataManipulator.FinalSearchResult) {
-			System.out.println(i);
-		}
+//		for (int i : DataManipulator.FinalSearchResult) {
+//			System.out.println(i);
+//		}
 
 		/*保存修改*/
 //		StorageAccessor.SaveDiscussionToCSV(Global.DefaultSavePath + "\\Book2.csv", "GB2312");
