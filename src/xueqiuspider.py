e37b0e6b192cbf321ae6bd7ea4af6fe143f3c425
lzk20182000 <1749977097@qq.com>
2021-05-26 21:01:54 +0800

    changeJob

--- src/xueqiuspider.py
+++ src/xueqiuspider.py
@@ -2,6 +2,7 @@ import requests
 import time
 import re
 import urllib3
+import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 proxies = {
@@ -34,8 +35,8 @@ def parse_comment_url( url):
         content_list.append(item)
     return content_list, count
 
-stock_num = 'SH000688'  # 输入股票代码
-for i in range(100):
+stock_num = sys.argv[1]  # 输入股票代码
+for i in range(5):
     detail_url = "https://xueqiu.com/statuses/search.json?count=10&comment=0&symbol={}&hl=0&source=all&sort=&page={}&q=&type=11".format(stock_num, i + 1)
     print("正爬取"+detail_url)
     try:
