d25536e04026175cf0de43abb7448c210dba23b1
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-05-26 23:57:03 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,18 @@ import java.util.Vector;
 
 public class Main {
 	public static void main(String[] args) throws XPathExpressionException, IOException, SAXException, InterruptedException {
-//		System.out.println(System.getProperty("java.version"));
-//		System.out.println(ManagementFactory.getRuntimeMXBean().getSpecVersion());
 		GUI MainForm = new GUI();
-//		GUIConfig guiConfig = new GUIConfig(Global.DefaultConfig);
-//		Config.LoadConfig(Global.DefaultConfig);
+
+		/*读取全部可用标注*/
 //		StorageAccessor.LoadAllAvailableLabels();
 //		for (Map.Entry<String, HashSet<String>> entry : DataManipulator.AllLabels.entrySet()) {
 //			System.out.println(entry.toString());
 //		}
 
+		/*保存全部可用标注*/
 //		StorageAccessor.SaveAllAvailableLabels();
 
+		/*读取指定文件中的股票讨论及其标注*/
 //		StorageAccessor.LoadDiscussionFromCSV(Global.DefaultSavePath + "\\xueqiu.csv", "gbk", 1);
 //		for (DiscussionItem entry : DataManipulator.DiscussionList) {
 //			System.out.println(entry.GetText());
@@ -31,17 +31,18 @@ public class Main {
 //			System.out.println(entry.toString());
 //		}
 
+		/*搜索功能*/
 //		DataManipulator.Search(2, null, null);
 //		DataManipulator.Search(0, new String[]{ "2A" }, null);
 //		DataManipulator.Search(0, null, new String[]{ "是" });
 
-		//while (DataManipulator.FinalSearchResult.size() == 0) ;
-//		Thread.sleep(5000);
+		/*最终的搜索结果*/
 //		System.out.println(DataManipulator.FinalSearchResult.size());
 //		for (int i : DataManipulator.FinalSearchResult) {
 //			System.out.println(i);
 //		}
 
+		/*保存修改*/
 //		StorageAccessor.SaveDiscussionToCSV(Global.DefaultSavePath + "\\Book2.csv", "GB2312");
 	}
 }
