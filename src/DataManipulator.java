35266f08231a11e65b249b51841d2527ff8c260f
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-05-27 23:59:24 +0800

    Update DataManipulator.java

--- src/DataManipulator.java
+++ src/DataManipulator.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 
 class DiscussionItem {
 	private String Text;
-	private ConcurrentHashMap<String, HashSet<String>> Labels;
+	private ConcurrentHashMap<String, HashMap<String, Integer>> Labels;
 
 	DiscussionItem() { Labels = new ConcurrentHashMap<>(); }
 
@@ -14,7 +14,7 @@ class DiscussionItem {
 
 	String GetText() { return Text; }
 
-	ConcurrentHashMap<String, HashSet<String>> GetLabels() { return Labels; }
+	ConcurrentHashMap<String, HashMap<String, Integer>> GetLabels() { return Labels; }
 
 	void SetText(String Text) { this.Text = Text; }
 }
@@ -44,27 +44,29 @@ public class DataManipulator {
 	}
 
 	public static void AddLabel(int Index, String Category, String Label) { // 为指定股票讨论添加新标签
-		ConcurrentHashMap<String, HashSet<String>> TargetLabels = DiscussionList.get(Index).GetLabels();
-		HashSet<String> TargetCat = TargetLabels.get(Category);
-		if (TargetCat == null) TargetLabels.put(Category, new HashSet<>());
-		TargetCat.add(Label);
+		ConcurrentHashMap<String, HashMap<String, Integer>> TargetLabels = DiscussionList.get(Index).GetLabels();
+		HashMap<String, Integer> TargetCat = TargetLabels.get(Category);
+		if (TargetCat == null) TargetLabels.put(Category, new HashMap<>());
+		Integer Count = TargetCat.get(Label);
+		if (Count == null) TargetCat.put(Label, 1);
+		else TargetCat.put(Label, Count + 1);
 	}
 
 	public static void ModifyLabel(int Index, String Category, String OldLabel, String NewLabel) { // 修改指定股票讨论的标签
-		ConcurrentHashMap<String, HashSet<String>> TargetLabels = DiscussionList.get(Index).GetLabels();
-		HashSet<String> TargetCat = TargetLabels.get(Category);
+		ConcurrentHashMap<String, HashMap<String, Integer>> TargetLabels = DiscussionList.get(Index).GetLabels();
+		HashMap<String, Integer> TargetCat = TargetLabels.get(Category);
 		TargetCat.remove(OldLabel);
 		TargetCat.add(NewLabel);
 	}
 
 	public static void DeleteLabel(int Index, String Category) { // 为指定股票讨论删除一类标签
-		ConcurrentHashMap<String, HashSet<String>> TargetLabels = DiscussionList.get(Index).GetLabels();
+		ConcurrentHashMap<String, HashMap<String, Integer>> TargetLabels = DiscussionList.get(Index).GetLabels();
 		TargetLabels.remove(Category);
 	}
 
 	public static void DeleteLabel(int Index, String Category, String Label) { // 为指定股票讨论删除一个标签
-		ConcurrentHashMap<String, HashSet<String>> TargetLabels = DiscussionList.get(Index).GetLabels();
-		HashSet<String> TargetCat = TargetLabels.get(Category);
+		ConcurrentHashMap<String, HashMap<String, Integer>> TargetLabels = DiscussionList.get(Index).GetLabels();
+		HashMap<String, Integer> TargetCat = TargetLabels.get(Category);
 		TargetCat.remove(Label);
 		if (TargetCat.size() == 0) TargetLabels.remove(Category);
 	}
@@ -199,9 +201,9 @@ public class DataManipulator {
 			if (Categories == null) { return; } // 该标签不属于任何已知的标签类（每个标签属于的类在读入全部可用标签与指定的股票讨论 CSV 文件时都会被登记），不符合条件
 			boolean FoundSingle = false;
 			for (String Category : Categories) { // 查找该条股票讨论是否包含该标签所属的某一个类；如果包含，则在类中查找
-				HashSet<String> LabelsOfThisCatOfThisItem = DiscussionList.get(index).GetLabels().get(Category);
+				HashMap<String, Integer> LabelsOfThisCatOfThisItem = DiscussionList.get(index).GetLabels().get(Category);
 				if (LabelsOfThisCatOfThisItem == null) { continue; }
-				if (LabelsOfThisCatOfThisItem.contains(Label) == true) { FoundSingle = true; break; }
+				if (LabelsOfThisCatOfThisItem.containsKey(Label) == true) { FoundSingle = true; break; }
 			}
 			if (FoundSingle == false) { Found = false; break; }
 		}
