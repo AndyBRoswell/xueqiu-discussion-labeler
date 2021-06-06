8c67842f677b7ab303b97a97e4ab1d34148ff8fa
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-06 11:14:24 +0800

    Update GUIAddLabelCategory.java

--- src/GUIAddLabelCategory.java
+++ src/GUIAddLabelCategory.java
@@ -14,6 +14,8 @@ public class GUIAddLabelCategory extends JFrame {
 	final JLabel lbLabel = new JLabel("标签");
 	final ArrayList<JTextField> tfLabels = new ArrayList<>();
 	final JButton btnOK = new JButton("确定");
+	final JButton btnAdd = new JButton("添加");
+	final JButton btnDelete = new JButton("删除");
 	final JButton btnCancel = new JButton("取消");
 
 	public GUIAddLabelCategory() {
@@ -21,10 +23,12 @@ public class GUIAddLabelCategory extends JFrame {
 		final Dimension Screen = Toolkit.getDefaultToolkit().getScreenSize();
 //		super.setLayout(null);
 		super.setLayout(new GridBagLayout());
-		super.setSize((int) Screen.getWidth() / 4, 100);
 		super.setLocationRelativeTo(null); // 先设置大小，再改变相对位置原点，否则窗口的左上角将位于屏幕中央
 		super.setResizable(false); // not resizable by the user
 		super.setTitle("添加新的一类标签");
+
+		// 预设 4 个标签填写框
+		for (int i = 0; i < 4; ++i) tfLabels.add(new JTextField());
 
 		// 动作监听程序
 		super.addComponentListener(new ComponentListener() {
@@ -41,7 +45,12 @@ public class GUIAddLabelCategory extends JFrame {
 					t.setBounds(x, y, X - x, h0);
 					y += h0;
 				}
-				
+				btnOK.setBounds(0, y, X / 4, h0);
+				btnAdd.setBounds(X / 4, y, X / 4, h0);
+				btnDelete.setBounds(X / 2, y, X / 4, h0);
+				btnCancel.setBounds(X * 3 / 4, y, X / 4, h0);
+
+				GUIAddLabelForm.setSize((int) Screen.getWidth() / 4, y + h0 + 40);
 			}
 
 			@Override public void componentMoved(ComponentEvent e) {}
@@ -57,7 +66,7 @@ public class GUIAddLabelCategory extends JFrame {
 		for (JTextField t : tfLabels) {
 			super.add(t);
 		}
-		super.add(btnOK); super.add(btnCancel);
+		super.add(btnOK); super.add(btnAdd); super.add(btnDelete); super.add(btnCancel);
 
 		// 显示
 		super.setVisible(true);
