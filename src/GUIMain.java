05ae59ef4ed49aeca219873971ab208d3f2c3332
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-10 15:01:16 +0800

    Update GUIMain.java

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -323,26 +323,26 @@ public class GUIMain extends JFrame {
 		TableModel = new DiscussionTableModel();
 		DiscussionTable = new JTable(TableModel);
 		DiscussionScrollPane = new JScrollPane(DiscussionTable);
-		TableModel.addTableModelListener(new TableModelListener() {
-			@Override public void tableChanged(TableModelEvent e) {
-				int Row = e.getFirstRow();
-				int MaxPreferredHeight = 0;
-				for (int j = 0; j < TableModel.getColumnCount(); ++j) {
-					Component comp = DiscussionTable.prepareRenderer(DiscussionTable.getCellRenderer(Row, j), Row, j);
-					MaxPreferredHeight = Math.max(MaxPreferredHeight, comp.getPreferredSize().height);
-				}
-				DiscussionTable.setRowHeight(Row, MaxPreferredHeight);
-			}
-		});
-//		DiscussionTable.setRowHeight(8 * Global.FontSizeD);
-		for (int i = 0; i < DiscussionTable.getRowCount(); ++i) {
-			int MaxPreferredHeight = 0;
-			for (int j = 0; j < TableModel.getColumnCount(); ++j) {
-				Component comp = DiscussionTable.prepareRenderer(DiscussionTable.getCellRenderer(i, j), i, j);
-				MaxPreferredHeight = Math.max(MaxPreferredHeight, comp.getPreferredSize().height);
-			}
-			DiscussionTable.setRowHeight(i, MaxPreferredHeight);
-		}
+//		TableModel.addTableModelListener(new TableModelListener() {
+//			@Override public void tableChanged(TableModelEvent e) {
+//				int Row = e.getFirstRow();
+//				int MaxPreferredHeight = 0;
+//				for (int j = 0; j < TableModel.getColumnCount(); ++j) {
+//					Component comp = DiscussionTable.prepareRenderer(DiscussionTable.getCellRenderer(Row, j), Row, j);
+//					MaxPreferredHeight = Math.max(MaxPreferredHeight, comp.getPreferredSize().height);
+//				}
+//				DiscussionTable.setRowHeight(Row, MaxPreferredHeight);
+//			}
+//		});
+////		DiscussionTable.setRowHeight(8 * Global.FontSizeD);
+//		for (int i = 0; i < DiscussionTable.getRowCount(); ++i) {
+//			int MaxPreferredHeight = 0;
+//			for (int j = 0; j < TableModel.getColumnCount(); ++j) {
+//				Component comp = DiscussionTable.prepareRenderer(DiscussionTable.getCellRenderer(i, j), i, j);
+//				MaxPreferredHeight = Math.max(MaxPreferredHeight, comp.getPreferredSize().height);
+//			}
+//			DiscussionTable.setRowHeight(i, MaxPreferredHeight);
+//		}
 		DiscussionTable.getSelectionModel().addListSelectionListener(new RowSelectionListener()); // 当选中股评时，可选标注面板显示各个标签被选中的数量
 		this.add(DiscussionScrollPane);
 		Refresh();
