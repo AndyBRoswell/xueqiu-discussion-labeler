11df6e1ca6a8682ff6170fc72abb8ab69dd71ea5
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-26 16:14:19 +0800

    Create GUIImportFiles.java

---
+++ src/GUIImportFiles.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class GUIImportFiles extends JFrame {
+	// 按钮
+	final JPanel ButtonPanel = new JPanel(new GridLayout(1, 4));
+	final JButton btnConfirmImport = new JButton("确认导入");
+	final JButton btnAddFiles = new JButton("添加文件");
+	final JButton btnDeleteFiles = new JButton("删除文件");
+	final JButton btnClearList = new JButton("清空列表");
+
+	// 导入的文件列表
+	final ArrayList<String> ImportedFiles = new ArrayList<>();
+	final FileTableModel FileListModel = new FileTableModel();
+	final JTable FileList = new JTable(FileListModel);
+	final JScrollPane FileListScrollPane = new JScrollPane(FileList);
+
+	class FileTableModel extends AbstractTableModel {
+		private final String[] FileListColumnNames = new String[]{ "文件名" };
+
+		@Override public int getRowCount() { return ImportedFiles.size(); }
+
+		@Override public int getColumnCount() { return 1; }
+
+		@Override public String getColumnName(int col) { return FileListColumnNames[col]; }
+
+		@Override public Object getValueAt(int rowIndex, int columnIndex) { return ImportedFiles.get(rowIndex); }
+
+		@Override public boolean isCellEditable(int row, int col) { return false; }
+
+		@Override public Class getColumnClass(int c) { return getValueAt(0, c).getClass(); }
+	}
+
+	// 加载导入文件窗口
+	public GUIImportFiles() {
+		// 窗体的基本属性
+		final Dimension Screen = Toolkit.getDefaultToolkit().getScreenSize();
+		super.setSize(Screen.width / 4, Screen.height / 4);
+		super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		super.setLocationRelativeTo(null);
+		super.getContentPane().setLayout(new GridBagLayout());
+
+		// 动作监听程序
+		
+
+		// 添加控件
+		ButtonPanel.add(btnConfirmImport);
+		ButtonPanel.add(btnAddFiles);
+		ButtonPanel.add(btnDeleteFiles);
+		ButtonPanel.add(btnClearList);
+		super.add(ButtonPanel);
+		super.add(FileListScrollPane);
+
+		// 显示
+		super.setVisible(true);
+	}
+}
