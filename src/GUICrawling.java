aae2f24d7c6160d722630368ecf40668a1b2adda
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-27 10:33:47 +0800

    Update GUICrawling.java

--- src/GUICrawling.java
+++ src/GUICrawling.java
@@ -1,12 +1,23 @@
+import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import java.awt.*;
 import java.util.ArrayList;
 
-public class GUICrawling {
+public class GUICrawling extends JFrame {
 	// 本窗体
 	final GUICrawling ThisForm = this;
 
+	// 按钮面板
+	final JPanel ButtonPanel = new JPanel(new GridBagLayout());
+	final JButton btnStartAll = new JButton("全部开始");
+
+	// 任务列表
+	TaskTableModel TaskListModel = new TaskTableModel();
+	JTable TaskList = new JTable(TaskListModel);
+	JScrollPane TaskListCrollPane = new JScrollPane(TaskList);
+
 	// 任务列表模型
-	class TaskTableModel extends AbstractTableModel {
+	static class TaskTableModel extends AbstractTableModel {
 		class TaskInfo {
 			String FileName;
 			int Status;
@@ -33,10 +44,22 @@ public class GUICrawling {
 
 		@Override public void setValueAt(Object value, int rowIndex, int ColumnIndex) { // 仅文件名可更改
 			Tasks.get(rowIndex).FileName = (String) value;
+			fireTableCellUpdated(rowIndex, ColumnIndex);
 		}
 
 		@Override public boolean isCellEditable(int row, int col) { return col == 0; } // 仅文件名可更改
 
 		@Override public Class getColumnClass(int c) { return getValueAt(0, c).getClass(); }
+	}
+
+	// 加载任务列表（仅在软件启动时调用）
+	public GUICrawling() {
+		// 窗体的基本属性
+		super.setDefaultCloseOperation(HIDE_ON_CLOSE); // 点击关闭按钮时隐藏窗口
+	}
+
+	// 显示任务列表
+	public void ShowTaskList(){
+		this.setVisible(true);
 	}
 }
