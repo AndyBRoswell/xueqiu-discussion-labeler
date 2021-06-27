f97afd575191d0423090578dafb9c16ab018c49b
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-27 11:27:21 +0800

    Update GUICrawling.java

--- src/GUICrawling.java
+++ src/GUICrawling.java
@@ -5,7 +5,9 @@ import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GUICrawling extends JFrame {
 	// 本窗体
@@ -17,10 +19,11 @@ public class GUICrawling extends JFrame {
 
 	// 任务列表
 	static class TaskInfo {
+		String TickerSymbol;
 		String FileName;
 		int Status;
 
-		TaskInfo(String FileName, int Status) { this.FileName = FileName; this.Status = Status; }
+		TaskInfo(String TickerSymbol, String FileName, int Status) { this.TickerSymbol = TickerSymbol; this.FileName = FileName; this.Status = Status; }
 	}
 	final ArrayList<TaskInfo> Tasks = new ArrayList<>();
 	final TaskTableModel TaskListModel = new TaskTableModel();
@@ -29,18 +32,19 @@ public class GUICrawling extends JFrame {
 
 	// 任务列表模型
 	class TaskTableModel extends AbstractTableModel {
-		private final String[] TaskListColumnNames = new String[]{ "文件名", "状态" };
+		private final String[] TaskListColumnNames = new String[]{ "股票代码", "文件名", "状态" };
 
 		@Override public int getRowCount() { return Tasks.size(); }
 
-		@Override public int getColumnCount() { return 2; }
+		@Override public int getColumnCount() { return 3; }
 
 		@Override public String getColumnName(int col) { return TaskListColumnNames[col]; }
 
 		@Override public Object getValueAt(int rowIndex, int columnIndex) {
 			return switch (columnIndex) {
-				case 0 -> Tasks.get(rowIndex).FileName;
-				case 1 -> Tasks.get(rowIndex).Status;
+				case 0 -> Tasks.get(rowIndex).TickerSymbol;
+				case 1 -> Tasks.get(rowIndex).FileName;
+				case 2 -> Tasks.get(rowIndex).Status;
 				default -> null;
 			};
 		}
@@ -50,7 +54,7 @@ public class GUICrawling extends JFrame {
 			fireTableCellUpdated(rowIndex, ColumnIndex);
 		}
 
-		@Override public boolean isCellEditable(int row, int col) { return col == 0; } // 仅文件名可更改
+		@Override public boolean isCellEditable(int row, int col) { return col != 2; } // 仅文件名可更改
 
 		@Override public Class getColumnClass(int c) { return getValueAt(0, c).getClass(); }
 	}
@@ -76,14 +80,23 @@ public class GUICrawling extends JFrame {
 		ButtonPanelLayout.weightx = 0; ButtonPanelLayout.weighty = 1;
 
 		// 表格的设置
-		TaskList.getColumnModel().getColumn(1).setMinWidth(80);
-		TaskList.getColumnModel().getColumn(1).setMaxWidth(80);
+		TaskList.getColumnModel().getColumn(0).setMinWidth(80);
+		TaskList.getColumnModel().getColumn(0).setMaxWidth(80);
+		TaskList.getColumnModel().getColumn(2).setMinWidth(80);
+		TaskList.getColumnModel().getColumn(2).setMaxWidth(80);
 
 		// 动作监听程序
 		btnStartAll.addActionListener(new ActionListener() {
 			@Override public void actionPerformed(ActionEvent e) {
 				for (TaskInfo t : Tasks) {
-					
+					new Thread(() -> {
+						try {
+							final Process p = Runtime.getRuntime().exec("python ");
+						}
+						catch (IOException ioException) {
+							ioException.printStackTrace();
+						}
+					}).start();
 				}
 			}
 		});
